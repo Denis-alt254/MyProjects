67e4b968a31f64f77ef647ec63a719f26fdc1e33
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-01-13 21:43:13 +0300

    Quick Update

---
+++ first.cpp
@@ -0,0 +1,29 @@
+/*
+cpp programe to check voting eligibility
+
+Author: Denis
+Reg no: BSE-05-0175/2024
+Date: 13/01/2025
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //Declare variables
+    string name;
+    int age;
+
+    //Prompt the user to enter his or her name
+    cout <<"Enter your name:"<<endl;
+    cin >> name;
+    cout <<"My name is "<<name;
+
+    //Prompt the user to enter his or her age
+    cout <<endl<<"Enter your age:"<<endl;
+    cin >> name;
+    cout <<"My age is "<<age;
+
+    return 0;
+}
