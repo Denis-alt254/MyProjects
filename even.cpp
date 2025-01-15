1bb9416cbe0d9ade7548b118893c125b9cfd4c9f
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-01-15 17:32:00 +0300

    Another

---
+++ even.cpp
@@ -0,0 +1,32 @@
+/*
+Cpp programe of identify an even number
+
+Author:Denis
+Reg no:BSE-05-0175/2024
+Date:15/01/2025
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Declare variables
+    int num;
+    int div;
+
+    //Prompt the user to enter a number
+    cout <<"Enter a number:"<<endl;
+    cin >>num;
+    cout <<endl<<"The number is "<<num<<endl;
+
+    div = num % 2;
+
+    if (div = 0 ){
+        cout <<endl<<"The number is even";
+    }else{
+        cout <<endl<<"The number is odd";
+    }
+
+    return 0;
+}
