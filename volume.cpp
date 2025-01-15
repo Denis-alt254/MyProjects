7266f48faea7c00df2245d1586eb177abab39fd9
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-01-15 19:31:36 +0300

    volume
    Another code to find the volume of a sphere.

---
+++ volume.cpp
@@ -0,0 +1,31 @@
+/*
+Cpp programe to find the volume of a sphere
+
+Author: Denis
+Reg no: BSE-05-0175/2024
+Date: 15/01/2025
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //Declare variables
+    int radius;
+    double volume;
+    const int Pi = 22/7;
+    string units = "cm";
+
+    //Prompt the user to enter the radius
+    cout <<"Enter the radius:"<<endl;
+    cin >>radius;
+    cout <<"Radius is: "<<radius<<units;
+
+    //calculate the volume
+    volume = 4/3 * Pi *radius^3;
+    cout <<endl<<"Volume of the sphere is: "<<volume<<units<<"^2";
+
+    return 0;
+}
