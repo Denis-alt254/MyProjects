fb328ab119fc95a3cf725a2dc03e3a2f53bd9c72
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-01-20 16:31:39 +0300

    Quick Update

---
+++ Functions.cpp
@@ -0,0 +1,30 @@
+/*
+Predifined Functions
+
+Author: Denis Kipkurui
+Reg no: BSE-05-0175/2024
+Date: 20/1/2025
+
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    //declare variables
+    int num;
+
+    //Prompt the user to enter a number
+    cout <<"Enter a number:"<<endl;
+    cin>>num;
+
+    //print the solutions
+    cout <<"Square root: "<<sqrt(num)<<endl;
+    cout <<"Tan :"<<tan(num)<<endl;
+    cout <<"Cos :"<<cos(num)<<endl;
+    cout <<"Round:"<<round(num)<<endl;
+
+    return 0;
+}
---
+++ UserFunctions.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+float addition(float x, int y){
+    float sum;
+    sum = x + y;
+    return sum;
+}
+
+// Function declaration
+float simple_interest(int principle, float rate, int time){
+    float interest;
+    interest = principle * rate * time;
+    return interest;
+}
+
+// Function declaration
+int even_num(int num){
+    return num % 2 == 0; // Return 1 if even, 0 if odd
+}
+
+int main(){
+    float sum, interest;
+    int num, even;
+
+    // Function calling
+    sum = addition(12.5, 56);
+    interest = simple_interest(50000, 0.14, 4);
+
+    // Print solutions
+    cout << "Sum: " << sum << endl;
+    cout << "Simple Interest: " << interest << endl;
+
+    // Prompt the user to enter a number
+    cout << "Enter a number:" << endl;
+    cin >> num;
+    cout << "Number: " << num << endl;
+
+    // Call the even_num function after user input
+    even = even_num(num);
+
+
+    // Check if the number is even or odd
+    if (even) {
+        cout << "Is an even number" << endl;
+    } else {
+        cout << "Is an odd number" << endl;
+    }
+
+    return 0;
+}
