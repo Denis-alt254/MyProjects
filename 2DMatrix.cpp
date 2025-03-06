c4609364df417c3e571019e472a8a8e5aebfe367
Denis Kipkurui <deniskipkurui2024@gmail.com>
2025-03-06 15:31:56 +0300

    Updated

--- 2DMatrix.cpp
+++ 2DMatrix.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main(){
     int i, j;
     int arr[2][3] = {{2, 3, 4},
-                    {2, 2, 2}};
+                    {2, 9, 7}};
 
                     for(i = 0; i < 2; i++){
                         for(j = 0; j< 3; j++){
